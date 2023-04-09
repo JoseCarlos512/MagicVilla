5ef45bf975d56672165b6d1afd2cb0760ec71bb8
Jose Carlos <ljosecarlos295@gmail.com>
2023-04-09 18:31:04 -0500

    Add EndPoint of type GET  (Testing)

--- MagicVilla_API/Controllers/VillaController.cs
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla_API.Modelos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
@@ -8,7 +9,14 @@ namespace MagicVilla_API.Controllers
     public class VillaController : ControllerBase
     {
 
-
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas() { 
+            return new List<Villa>
+            {
+                new Villa{ Id=1, Nombre="Vista a la Piscina" },
+                new Villa{ Id=2, Nombre="Vista a la Playa" }
+            };
+        }
 
     }
 }
